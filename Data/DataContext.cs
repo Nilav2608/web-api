ac33194c6fb39ec8c6a86e81a295afbebf4b0400
Nilavarasu2608 <knlsu444@gmail.com>
2023-09-30 20:48:39 +0530

    DataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using web_api.models;
+
+namespace web_api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        
+
+        // to query and set characters
+        public DbSet<Character> Characters => Set<Character>();
+    }
+}
