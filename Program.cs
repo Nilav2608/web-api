767e460e786775ecd0a003f083ebad83ab594394
Nilavarasu2608 <knlsu444@gmail.com>
2023-10-01 14:08:34 +0530

    enabling databse conncetions and migrations

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.Extensions.Options;
+using web_api.Data;
 using web_api.Services.CharacterServices;
-
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
